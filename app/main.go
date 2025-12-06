7fc3f6e6f65ca70ed7a4a8d7ba14710b84913bf2
Niraj Shaw <nirazshaw156@gmail.com>
2025-12-06 14:52:16 +0530

    [any message]

--- app/main.go
+++ app/main.go
@@ -10,26 +10,31 @@ import (
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Print
 
+func getCmdAndArg(input string) (string, string) {
+	cmd, arg, _ := strings.Cut(input, " ")
+	return cmd, arg
+}
+
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
 
 	for {
 		fmt.Print("$ ")
 
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			fmt.Printf("cound not read the command: %s", err)
 		}
 
-		command = strings.TrimSpace(command)
-		fmt.Println(len(command))
-		fmt.Println(command)
+		command, argument := getCmdAndArg(input)
 
-		if command == "exit" {
-			break
+		switch command {
+		case "exit":
+			return
+		case "echo":
+			fmt.Println(argument)
+		default:
+			fmt.Println(command + ": command not found")
 		}
-
-		fmt.Println(command + ": command not found")
-
 	}
 }
