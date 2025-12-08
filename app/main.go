e1badfe1c06e1089130f4afb69776604378b0ee5
Niraj Shaw <nirazshaw156@gmail.com>
2025-12-08 13:01:36 +0530

    [any message]

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 )
@@ -35,48 +36,72 @@ func findExecutable(cmd string) (string, bool) {
 	return "", false
 }
 
-func isBuiltInCmds(args []string) string {
-	builtInCommands := []string{"type", "echo", "exit"}
+func isBuiltin(cmd string) bool {
+	builtins := map[string]bool{
+		"type": true,
+		"echo": true,
+		"exit": true,
+	}
+	return builtins[cmd]
+}
 
-	// check for builtin commands
-	for _, elem := range builtInCommands {
-		if args[0] == elem {
-			return fmt.Sprintf("%s is a shell builtin", args)
-		}
+func commandInfo(cmd string) string {
+	// check for builin
+	if isBuiltin(cmd) {
+		return fmt.Sprintf("%s is a shell builtin", cmd)
+	}
+	//  check for executbale files
+	if exe, found := findExecutable(cmd); found {
+		return fmt.Sprintf("%s is %s", cmd, exe)
 	}
 
-	// check for executable files
-	fullPath, found := findExecutable(args[0])
-	if found {
-		return fmt.Sprintf("%s is %s", args[0], fullPath)
-	}
-
-	return fmt.Sprintf("%s not found", args[0])
+	return fmt.Sprintf("%s not found", cmd)
 }
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
 
 	for {
 		fmt.Print("$ ")
 
-		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		line, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Printf("cound not read the command: %s", err)
+			continue
 		}
-		input = strings.TrimSpace(input)
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
 
-		command, arguments := parseInput(input)
+		command, arguments := parseInput(line)
 
 		switch command {
 		case "type":
-			output := isBuiltInCmds(arguments)
+			output := commandInfo(arguments[0])
 			fmt.Println(output)
+
 		case "echo":
 			fmt.Println(arguments)
+
 		case "exit":
 			return
+
 		default:
-			fmt.Println(command + ": command not found")
+
+			executableFile, found := findExecutable(command)
+			if !found {
+				fmt.Println(command + ": command not found")
+				continue
+			}
+
+			cmd := exec.Command(executableFile, arguments...)
+			cmd.Stdin = os.Stdin
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+
+			cmd.Run()
+
 		}
 	}
 }
